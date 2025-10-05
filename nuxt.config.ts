3a739bd85c191bb91995085be29ae8781826488b
Mæhæmad <alkaitagi@outlook.com>
2025-10-05 14:57:36 +0000

    Extract styles into a file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+  css: ['~/assets/css/main.css'],
   router: {
     options: {
       strict: true
