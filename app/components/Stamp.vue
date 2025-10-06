7476840b1b4eee505f5f87d7f9844cddf6a4e6aa
Mæhæmad <alkaitagi@outlook.com>
2025-10-06 16:29:05 +0000

    Add stamp animation

--- app/components/Stamp.vue
+++ app/components/Stamp.vue
@@ -1,16 +1,29 @@
 <template>
-  <img src="/stamp.png" alt="" class="stamp" aria-hidden="true">
+  <div class="stamp-wrapper">
+    <img src="/stamp.png" alt="" class="stamp" aria-hidden="true" draggable="false">
+  </div>
 </template>
 
 <style scoped>
-.stamp {
-  display: block;
+.stamp-wrapper {
   float: right;
   margin: 2rem 0 1rem 2rem;
   width: calc(65ch / 3);
+  transition: 0.2s;
+  cursor: pointer;
   opacity: 0.3;
-  filter: invert(1);
   transform: rotate(15deg);
+
+  &:hover {
+    transform: rotate(0);
+    opacity: 1;
+  }
+}
+
+.stamp {
+  display: block;
+  width: 100%;
+  filter: invert(1);
   user-select: none;
   pointer-events: none;
 }
