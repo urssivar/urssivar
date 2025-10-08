408df83ea22cd1e4a9a7a5b30609882923d906c4
Mæhæmad <alkaitagi@outlook.com>
2025-10-08 22:26:04 +0000

    Change stamp interaction

--- app/components/Stamp.vue
+++ app/components/Stamp.vue
@@ -1,10 +1,13 @@
+<script setup lang="ts">
+const fixed = ref(false);
+</script>
+
 <template>
-  <div class="mt-16 mb-8 mx-auto w-[152px] cursor-pointer relative group select-none">
-    <img src="/stamp-text.svg" alt=""
-      class="block w-full invert-[88%] dark:invert-[11%] pointer-events-none animate-[spin_20s_linear_infinite]"
-      aria-hidden="true">
-    <img src="/stamp-logo.svg" alt=""
-      class="absolute top-0 left-0 block w-full invert-[88%] dark:invert-[11%] pointer-events-none rotate-[15deg] group-hover:rotate-0 group-active:rotate-0 transition-all duration-300 ease-out"
-      aria-hidden="true">
+  <div @click="fixed = true" class="mt-16 mb-8 mx-auto w-[152px] cursor-pointer relative select-none">
+    <img src="/stamp-text.svg" alt="Urssivar: Kaitag Studies"
+      class="block w-full invert-[88%] dark:invert-[12%] pointer-events-none animate-[spin_20s_linear_infinite]">
+    <img src="/stamp-logo.svg" alt="Urssivar logo"
+      class="absolute top-0 left-0 block w-full invert-[88%] dark:invert-[12%] pointer-events-none rotate-12 transition ease-out"
+      :class="{ 'rotate-none': fixed }">
   </div>
 </template>
