95f64112e62b065537340070ba9b2599c69074ef
Mæhæmad <alkaitagi@outlook.com>
2025-10-08 20:25:12 +0000

    Fix stamp animation on touch

--- app/components/Stamp.vue
+++ app/components/Stamp.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="mt-16 mb-8 mx-auto w-[152px] cursor-pointer opacity-50 hover:opacity-100 focus-within:opacity-100 relative transition-opacity duration-300 group">
+    class="mt-16 mb-8 mx-auto w-[152px] cursor-pointer opacity-50 hover:opacity-100 active:opacity-100 focus-within:opacity-100 relative transition-opacity duration-300 group">
     <img src="/stamp-text.svg" alt=""
       class="block w-full invert dark:invert-0 select-none pointer-events-none animate-[spin_20s_linear_infinite]"
       aria-hidden="true" draggable="false">
     <img src="/stamp-logo.svg" alt=""
-      class="absolute top-0 left-0 block w-full invert dark:invert-0 select-none pointer-events-none rotate-[15deg] group-hover:rotate-0 transition-transform duration-300 ease-out"
+      class="absolute top-0 left-0 block w-full invert dark:invert-0 select-none pointer-events-none rotate-[15deg] group-hover:rotate-0 group-active:rotate-0 transition-transform duration-300 ease-out"
       aria-hidden="true" draggable="false">
   </div>
 </template>
