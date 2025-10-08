749744a41c090fd363bdfc66643795c9dea46e56
Mæhæmad <alkaitagi@outlook.com>
2025-10-08 21:35:22 +0000

    Clean up

--- app/components/Stamp.vue
+++ app/components/Stamp.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="mt-16 mb-8 mx-auto w-[152px] cursor-pointer relative group">
+  <div class="mt-16 mb-8 mx-auto w-[152px] cursor-pointer relative group select-none">
     <img src="/stamp-text.svg" alt=""
-      class="block w-full invert-[88%] dark:invert-[11%] select-none pointer-events-none animate-[spin_20s_linear_infinite]"
-      aria-hidden="true" draggable="false">
+      class="block w-full invert-[88%] dark:invert-[11%] pointer-events-none animate-[spin_20s_linear_infinite]"
+      aria-hidden="true">
     <img src="/stamp-logo.svg" alt=""
-      class="absolute top-0 left-0 block w-full invert-[88%] dark:invert-[11%] select-none pointer-events-none rotate-[15deg] group-hover:rotate-0 group-active:rotate-0 transition-all duration-300 ease-out"
-      aria-hidden="true" draggable="false">
+      class="absolute top-0 left-0 block w-full invert-[88%] dark:invert-[11%] pointer-events-none rotate-[15deg] group-hover:rotate-0 group-active:rotate-0 transition-all duration-300 ease-out"
+      aria-hidden="true">
   </div>
 </template>
