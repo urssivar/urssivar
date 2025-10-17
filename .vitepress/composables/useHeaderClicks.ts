77a6d9f336d5888dcc941c8f6a702ac672269bda
Mæhæmad <alkaitagi@outlook.com>
2025-10-17 03:00:43 +0000

    Simplify headers

--- .vitepress/composables/useHeaderClicks.ts
+++ .vitepress/composables/useHeaderClicks.ts
@@ -2,19 +2,24 @@ import { onContentUpdated } from 'vitepress';
 
 export function useHeaderClicks() {
     const addHeaderListeners = () => {
-        document.querySelectorAll(':is(h1, h2, h3, h4, h5, h6)[id]')
-            .forEach(header => {
-                header.removeEventListener('click', handler);
-                header.addEventListener('click', handler);
+        document.querySelectorAll(':is(h2, h3, h4)[id]::before')
+            .forEach(anchor => {
+                anchor.removeEventListener('click', handler);
+                anchor.addEventListener('click', handler);
             });
     };
 
     const handler = (e: Event) => {
-        const target = e.target as HTMLElement | null;
-        if (!target || target.tagName === 'A') return;
+        const target = e.target as HTMLElement;
+        const header = target?.closest(':is(h2, h3, h4)');
+        if (!header) return;
 
-        target.scrollIntoView({ behavior: 'smooth' });
-        window.location.hash = target.id;
+        const mouseEvent = e as MouseEvent;
+        console.log(mouseEvent.offsetX);
+        if (mouseEvent.offsetX > 0) return;
+
+        header.scrollIntoView({ behavior: 'smooth' });
+        window.location.hash = header.id;
     };
 
     onContentUpdated(addHeaderListeners);
