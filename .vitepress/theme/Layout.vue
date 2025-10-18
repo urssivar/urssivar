8b61e56ab1619d2a87d1a7a555619542dab7e705
Mæhæmad <alkaitagi@outlook.com>
2025-10-18 15:48:54 +0000

    Stick footer to bottom

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -45,77 +45,80 @@ const isLanguageSubsection = computed(() => {
 
 <template>
   <UApp>
-    <NavBar>
-      <template #leading>
-        <Link :to="homeLink" class="flex gap-1.5 items-center text-default decoration-transparent">
-        <img src="/favicon-dark.svg" alt="Urssivar logo"
-          class="mx-1 size-6 invert-[88%] dark:invert-[12%] select-none pointer-events-none">
-        <span v-if="!isHome" class="font-bold text-lg">
-          Urssivar
+    <div class="min-h-screen flex flex-col">
+      <NavBar>
+        <template #leading>
+          <Link :to="homeLink" class="flex gap-1.5 items-center text-default decoration-transparent">
+          <img src="/favicon-dark.svg" alt="Urssivar logo"
+            class="mx-1 size-6 invert-[88%] dark:invert-[12%] select-none pointer-events-none">
+          <span v-if="!isHome" class="font-bold text-lg">
+            Urssivar
+          </span>
+          </Link>
+        </template>
+        <template #trailing>
+          <UButton icon="i-material-symbols:search-rounded" />
+          <Link :to="langLink">
+          <UButton icon="i-material-symbols:translate-rounded" />
+          </Link>
+        </template>
+      </NavBar>
+
+      <NavBar v-if="isLanguageSubsection" class="lg:hidden sticky top-0 z-10 bg-default/75 backdrop-blur-sm shadow-xs">
+        <template #leading>
+          <UDrawer direction="left" :handle="false"
+            :ui="{ content: 'w-2/3 sm:w-80 rounded-none p-4 pr-8', overlay: 'backdrop-blur-sm' }">
+            <UButton icon="i-material-symbols:menu-rounded" />
+            <template #content>
+              <SidebarNav class="w-full mt-12 mb-24" />
+            </template>
+          </UDrawer>
+        </template>
+        <span class="font-semibold flex-1 text-center">
+          Грамматика
         </span>
-        </Link>
-      </template>
-      <template #trailing>
-        <UButton icon="i-material-symbols:search-rounded" />
-        <Link :to="langLink">
-        <UButton icon="i-material-symbols:translate-rounded" />
-        </Link>
-      </template>
-    </NavBar>
+        <template #trailing>
+          <UDrawer direction="right" :handle="false"
+            :ui="{ content: 'w-2/3 sm:w-80 rounded-none p-8', overlay: 'backdrop-blur-sm' }">
+            <UButton icon="i-material-symbols:toc-rounded" />
+            <template #content>
+              <TableOfContents class="w-full mt-12 mb-24 text-sm!" />
+            </template>
+          </UDrawer>
+        </template>
+      </NavBar>
 
-    <NavBar v-if="isLanguageSubsection" class="lg:hidden sticky top-0 z-10 bg-default/75 backdrop-blur-sm shadow-xs">
-      <template #leading>
-        <UDrawer direction="left" :handle="false"
-          :ui="{ content: 'w-2/3 sm:w-80 rounded-none p-4 pr-8', overlay: 'backdrop-blur-sm' }">
-          <UButton icon="i-material-symbols:menu-rounded" />
-          <template #content>
-            <SidebarNav class="w-full mt-12 mb-24" />
-          </template>
-        </UDrawer>
-      </template>
-      <span class="font-semibold flex-1 text-center">
-        Грамматика
-      </span>
-      <template #trailing>
-        <UDrawer direction="right" :handle="false"
-          :ui="{ content: 'w-2/3 sm:w-80 rounded-none p-8', overlay: 'backdrop-blur-sm' }">
-          <UButton icon="i-material-symbols:toc-rounded" />
-          <template #content>
-            <TableOfContents class="w-full mt-12 mb-24 text-sm!" />
-          </template>
-        </UDrawer>
-      </template>
-    </NavBar>
+      <div v-if="isLanguageSubsection" class="mt-12 mb-24 lg:px-4 grid grid-cols-1 lg:grid-cols-[1fr_65ch_1fr]">
+        <aside class="hidden lg:block">
+          <SidebarNav class="sticky top-12" />
+        </aside>
+        <Content class="content-container w-full" />
+        <aside class="hidden lg:block">
+          <TableOfContents class="sticky top-12" />
+        </aside>
+      </div>
+      <Content v-else class="mt-12 mb-24" :class="{
+        'content-container': !(isHome || frontmatter.wide),
+        'mb-24': !isHome
+      }" />
 
-    <div v-if="isLanguageSubsection" class="mt-12 mb-24 lg:px-4 grid grid-cols-1 lg:grid-cols-[1fr_65ch_1fr]">
-      <aside class="hidden lg:block">
-        <SidebarNav class="sticky top-12" />
-      </aside>
-      <Content class="content-container w-full" />
-      <aside class="hidden lg:block">
-        <TableOfContents class="sticky top-12" />
-      </aside>
+      <div class="flex-1" />
+      <NavBar class="border-b-0 border-t">
+        <template #leading>
+          <span class="text-xs">Лицензия CC BY 4.0</span>
+        </template>
+        <template #trailing>
+          <Link to="https://t.me/urssivar">
+          <UButton icon="i-ix:telegram-logo" />
+          </Link>
+          <Link to="https://youtube.com/@urssivar">
+          <UButton icon="i-ix:youtube-filled" />
+          </Link>
+          <Link to="https://github.com/urssivar">
+          <UButton icon="i-ix:github-logo" />
+          </Link>
+        </template>
+      </NavBar>
     </div>
-    <Content v-else class="mt-12 mb-24" :class="{
-      'content-container': !(isHome || frontmatter.wide),
-      'mb-24': !isHome
-    }" />
-
-    <NavBar class="border-b-0 border-t">
-      <template #leading>
-        <span class="text-xs">Лицензия CC BY 4.0</span>
-      </template>
-      <template #trailing>
-        <Link to="https://t.me/urssivar">
-        <UButton icon="i-ix:telegram-logo" />
-        </Link>
-        <Link to="https://youtube.com/@urssivar">
-        <UButton icon="i-ix:youtube-filled" />
-        </Link>
-        <Link to="https://github.com/urssivar">
-        <UButton icon="i-ix:github-logo" />
-        </Link>
-      </template>
-    </NavBar>
   </UApp>
 </template>
--- src/ru/language/grammar/htest.md
+++ src/ru/language/grammar/htest.md
@@ -2,14 +2,4 @@
 numbered: true
 ---
 
-# heyy
-
-## heyy
-
-### Heyy
-
-### heyyy
-
 #### Heyyy rfrfrff rfrfefe ferf rferf erferf rerferf
-
-## whoa
