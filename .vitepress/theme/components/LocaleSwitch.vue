44c56d31c26cd344585cb4aa36bafeaefdae581b
Mæhæmad <alkaitagi@outlook.com>
2025-10-19 15:16:22 +0000

    Add home & locale components

---
+++ .vitepress/theme/components/LocaleSwitch.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useData, useRouter } from 'vitepress';
+import Link from '@/components/Link.vue';
+
+const { lang } = useData();
+const router = useRouter();
+
+const langLink = computed(() => {
+  const path = router.route.path;
+  return lang.value === 'ru'
+    ? path.replace('/ru', '')
+    : '/ru' + path;
+});
+</script>
+
+<template>
+  <Link :to="langLink">
+  <UButton icon="i-material-symbols:translate-rounded" />
+  </Link>
+</template>
