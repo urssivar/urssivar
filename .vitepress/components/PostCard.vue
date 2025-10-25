6cb4b1a72fb1a20d776795cc1de11a33739e0d71
Mæhæmad <alkaitagi@outlook.com>
2025-10-25 04:52:29 +0000

    Add id to post card header

--- .vitepress/components/PostCard.vue
+++ .vitepress/components/PostCard.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
+import { dateString } from '@/utils';
 import { type ContentData } from 'vitepress';
+import { computed } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   page: ContentData
 }>();
+
+const id = computed(() => {
+  const url = props.page.url.split('/');
+  return url[url.length - 1].split('.')[0];
+})
 </script>
 
 <template>
   <div class="my-8">
     <h4 class="title mt-0">
-      <a :href="page.url">{{ page.frontmatter.title }}</a>
+      <a :id :href="page.url">{{ page.frontmatter.title }}</a>
     </h4>
     <p>
       <span class="text-toned mr-[0.5em]">
-        {{ page.frontmatter.date.toString().substring(0, 10) }}
+        {{ dateString(page.frontmatter.date) }}
       </span>
       {{ page.frontmatter.description }}
     </p>
   </div>
 </template>
-
-<style scoped>
-.title::before {
-  content: none;
-}
-</style>
