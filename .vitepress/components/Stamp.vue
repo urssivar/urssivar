e6cad8f3830330e5892438cf2b4c1f006e16bbcb
Mæhæmad <alkaitagi@outlook.com>
2026-01-13 20:33:24 +0000

    Clean up stamp

--- .vitepress/components/Stamp.vue
+++ .vitepress/components/Stamp.vue
@@ -5,22 +5,20 @@ const fixed = ref(false);
 </script>
 
 <template>
-  <figure>
-    <div
-      @mousedown="fixed = true"
-      class="mx-auto my-2 size-36 cursor-pointer relative select-none"
-    >
-      <img
-        src="/stamp-text.svg"
-        alt="Urssivar: Kaitag Studies"
-        class="block w-full invert-90 dark:invert-0 pointer-events-none animate-[spin_20s_linear_infinite] print:animate-none"
-      />
-      <img
-        src="/stamp-logo.svg"
-        alt="Urssivar logo"
-        class="absolute top-0 left-0 block w-full invert-90 dark:invert-0 pointer-events-none rotate-12 print:rotate-none transition ease-out"
-        :class="{ 'rotate-none': fixed }"
-      />
-    </div>
-  </figure>
+  <div
+    @mousedown="fixed = true"
+    class="mx-auto my-12 size-36 cursor-pointer relative select-none"
+  >
+    <img
+      src="/stamp-text.svg"
+      alt="Urssivar: Kaitag Studies"
+      class="block w-full invert-90 dark:invert-0 pointer-events-none animate-[spin_20s_linear_infinite] print:animate-none"
+    />
+    <img
+      src="/stamp-logo.svg"
+      alt="Urssivar logo"
+      class="absolute top-0 left-0 block w-full invert-90 dark:invert-0 pointer-events-none rotate-12 print:rotate-none transition ease-out"
+      :class="{ 'rotate-none': fixed }"
+    />
+  </div>
 </template>
