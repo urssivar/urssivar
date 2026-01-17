57ca973d54e7dadd337ea45639eee627e8acf13c
Mæhæmad <alkaitagi@outlook.com>
2026-01-17 10:14:33 +0000

    Align NavBar left

--- .vitepress/theme/components/NavBar.vue
+++ .vitepress/theme/components/NavBar.vue
@@ -3,9 +3,9 @@
     <div class="mx-auto w-full md:w-[calc(65ch)] print:w-full">
       <nav class="px-6 sm:px-12 md:px-0 -mx-1.5 flex items-center gap-4 h-16">
         <slot name="leading" />
-        <div class="grow" />
-        <slot />
-        <div class="grow" />
+        <div class="flex flex-row grow">
+          <slot />
+        </div>
         <slot name="trailing" />
       </nav>
     </div>
