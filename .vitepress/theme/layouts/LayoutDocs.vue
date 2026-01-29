527b9ec39e6a8f5f80eba59e00e7af4fcf9a7372
Mæhæmad <alkaitagi@outlook.com>
2026-01-29 15:12:33 +0000

    Clean up layout & styles

--- .vitepress/theme/layouts/LayoutDocs.vue
+++ .vitepress/theme/layouts/LayoutDocs.vue
@@ -9,7 +9,6 @@ import Prose from "../components/Prose.vue";
 import DictionaryIndex from "@/components/DictionaryIndex.vue";
 import { useI18n } from "@/composables/i18n";
 import { useNav } from "@/composables/nav";
-import LayoutBase from "./LayoutBase.vue";
 import Home from "../components/Home.vue";
 import SearchButton from "../components/SearchButton.vue";
 import LocaleSwitch from "../components/LocaleSwitch.vue";
@@ -35,76 +34,84 @@ const tocRef = useTemplateRef("toc");
 </script>
 
 <template>
-  <div v-if="!isWide" class="print:hidden border-b sticky top-0 z-10 sheet">
-    <Prose>
-      <Toolbar class="h-16! -ml-1.5">
-        <template #leading>
-          <USlideover :close="false" side="left" v-model:open="menuOpen">
-            <UTooltip :text="t('nav.menu')">
-              <UButton
-                icon="i-material-symbols:menu"
-                :aria-label="t('nav.menu')"
-              />
-            </UTooltip>
-            <template #body>
-              <UButton
-                icon="i-material-symbols:close"
-                @click="menuOpen = false"
-                class="absolute -right-12 top-4 shadow-sm border border-default bg-default"
-              />
-              <Toolbar class="mx-1.5">
-                <template #leading>
-                  <Home />
-                </template>
-                <template #trailing>
-                  <LocaleSwitch />
-                </template>
-              </Toolbar>
-              <SidebarNav>
-                <DictionaryIndex
-                  v-if="nav.section?.path === 'dictionary'"
-                  mode="sidebar"
+  <div class="min-h-screen flex flex-col bg-muted dark:bg-default">
+    <div v-if="!isWide" class="print:hidden border-b sticky top-0 z-10 sheet">
+      <Prose>
+        <Toolbar class="h-16! -ml-1.5">
+          <template #leading>
+            <USlideover :close="false" side="left" v-model:open="menuOpen">
+              <UTooltip :text="t('nav.menu')">
+                <UButton
+                  icon="i-material-symbols:menu"
+                  :aria-label="t('nav.menu')"
                 />
-              </SidebarNav>
-              <Footer class="mt-auto" />
-            </template>
-          </USlideover>
+              </UTooltip>
+              <template #body>
+                <UButton
+                  icon="i-material-symbols:close"
+                  @click="menuOpen = false"
+                  class="absolute -right-12.5 top-4 shadow-sm border border-default bg-default"
+                />
+                <Toolbar class="mx-1.5">
+                  <template #leading>
+                    <Home />
+                  </template>
+                  <template #trailing>
+                    <LocaleSwitch />
+                  </template>
+                </Toolbar>
+                <SidebarNav>
+                  <DictionaryIndex
+                    v-if="nav.section?.path === 'dictionary'"
+                    mode="sidebar"
+                  />
+                </SidebarNav>
+                <Footer class="mt-auto" />
+              </template>
+            </USlideover>
+          </template>
+          <span class="font-semibold flex-1 text-center">
+            {{ nav.section?.text }}
+          </span>
+          <template #trailing>
+            <USlideover
+              :close="false"
+              v-if="tocRef?.isVisible"
+              side="right"
+              v-model:open="tocOpen"
+            >
+              <UTooltip :text="t('nav.toc')">
+                <UButton
+                  icon="i-material-symbols:toc"
+                  :aria-label="t('nav.toc')"
+                />
+              </UTooltip>
+              <template #body>
+                <UButton
+                  icon="i-material-symbols:close"
+                  @click="tocOpen = false"
+                  class="absolute -left-12 top-4 shadow-sm border border-default bg-default"
+                />
+                <TableOfContents class="text-sm!" />
+              </template>
+            </USlideover>
+            <SearchButton />
+          </template>
+        </Toolbar>
+      </Prose>
+    </div>
+    <Prose v-else class="mt-16 z-20 print:hidden">
+      <Toolbar class="-mx-1.5">
+        <template #leading>
+          <Home />
         </template>
-        <span class="font-semibold flex-1 text-center">
-          {{ nav.section?.text }}
-        </span>
         <template #trailing>
-          <USlideover
-            :close="false"
-            v-if="tocRef?.isVisible"
-            side="right"
-            v-model:open="tocOpen"
-          >
-            <UTooltip :text="t('nav.toc')">
-              <UButton
-                icon="i-material-symbols:toc"
-                :aria-label="t('nav.toc')"
-              />
-            </UTooltip>
-            <template #body>
-              <UButton
-                icon="i-material-symbols:close"
-                @click="tocOpen = false"
-                class="absolute -left-12 top-4 shadow-sm border border-default bg-default"
-              />
-              <TableOfContents class="text-sm!" />
-            </template>
-          </USlideover>
           <SearchButton />
+          <LocaleSwitch />
         </template>
       </Toolbar>
     </Prose>
-  </div>
 
-  <LayoutBase>
-    <template #header v-if="!isWide">
-      <div />
-    </template>
     <div
       class="grid md:mt-16 lg:mt-5.5 lg:mx-2 print:m-0 gap-4 grid-cols-1 lg:grid-cols-[1fr_auto_1fr] print:block"
     >
@@ -130,21 +137,18 @@ const tocRef = useTemplateRef("toc");
         <TableOfContents ref="toc" compact />
       </aside>
     </div>
-    <template #footer v-if="!isSheet">
-      <div />
-    </template>
-  </LayoutBase>
+
+    <Prose v-if="isSheet" class="print:hidden mt-auto">
+      <Footer class="mt-16 mb-4" />
+    </Prose>
+  </div>
 </template>
 
 <style lang="css" scoped>
 @reference "@/theme/styles/index.css";
 
-.sheet {
-  @apply shadow-sm print:shadow-none border-default print:border-none bg-default dark:bg-[#1e1e1e];
-}
-
 main {
-  @apply mx-auto w-full md:w-[calc(65ch+var(--spacing)*32)] print:w-full;
+  @apply mx-auto w-full md:w-[calc(65ch+var(--spacing)*32)] print:w-full print:px-[0.75in];
 
   article {
     @apply my-20;
@@ -152,7 +156,11 @@ main {
 }
 
 :deep(article > div > div > :not(.breakout)) {
-  @apply mx-6 sm:mx-12 md:mx-16;
+  @apply mx-6 sm:mx-12 md:mx-16 print:mx-0;
+}
+
+:deep(.breakout) {
+  @apply print:max-w-none print:-mx-[0.75in] print:px-[0.75in];
 }
 
 aside {
@@ -176,5 +184,42 @@ aside {
       @apply hidden;
     }
   }
+}
+
+.sheet {
+  @apply shadow-sm border-default bg-default dark:bg-[#1e1e1e] print:shadow-none print:border-none;
+}
+</style>
+
+<style lang="css">
+@reference "@/theme/styles/index.css";
+
+@page {
+  margin: 1in 0;
+}
+
+@page :first {
+  margin-top: 0;
+}
+
+@page :left {
+  @bottom-left {
+    content: counter(page);
+    @apply text-xs text-dimmed mx-[0.75in];
+  }
+}
+
+@page :right {
+  @bottom-right {
+    content: counter(page);
+    @apply text-xs text-dimmed mx-[0.75in];
+  }
+}
+
+aside,
+button,
+[role="button"],
+.header-anchor {
+  @apply print:hidden!;
 }
 </style>
