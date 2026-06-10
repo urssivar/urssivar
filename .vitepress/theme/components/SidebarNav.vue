ca8c687511b40fbf1eaca4053fbccecf843c050c
Mæhæmad <alkaitagi@outlook.com>
2026-06-10 16:55:00 +0000

    Put mission link to the top of the SidebarNav

--- .vitepress/theme/components/SidebarNav.vue
+++ .vitepress/theme/components/SidebarNav.vue
@@ -7,62 +7,60 @@ const nav = useNav();
 <template>
   <nav class="navlinks text-sm">
     <a
-      v-if="nav.module"
-      :href="nav.module.url"
-      :class="{ active: nav.module == nav.article }"
+      :href="nav.home.url"
+      :class="{ active: !nav.module, 'mb-8': nav.module }"
     >
-      {{ nav.module.text }}
+      {{ nav.home.text }}
     </a>
 
-    <template v-for="s in nav.module?.children" :key="s.url">
-      <a :href="s.url" :class="{ active: s == nav.article }">
-        <span class="block ml-4">
-          {{ s.text }}
-        </span>
+    <template v-if="nav.module">
+      <a :href="nav.module.url" :class="{ active: nav.module == nav.article }">
+        {{ nav.module.text }}
       </a>
 
-      <template v-if="s == nav.section">
-        <a
-          v-for="a in nav.section.children"
-          :key="a.url"
-          :href="a.url"
-          :class="{ active: a == nav.article }"
-        >
-          <span class="block ml-8">
-            {{ a.text }}
+      <template v-for="s in nav.module?.children" :key="s.url">
+        <a :href="s.url" :class="{ active: s == nav.article }">
+          <span class="block ml-4">
+            {{ s.text }}
           </span>
         </a>
-        <div class="ml-8">
-          <slot />
-        </div>
-      </template>
-    </template>
 
-    <template v-if="(nav.home.children?.length ?? 0) > 1">
-      <div v-if="nav.module" class="mt-8" />
-      <a v-else :href="nav.home.url" class="active">
-        {{ nav.home.text }}
-      </a>
-      <template v-for="m in nav.home.children" :key="m.url">
-        <template v-if="m != nav.module">
-          <a :href="m.url">
-            {{ m.text }}
-          </a>
+        <template v-if="s == nav.section">
           <a
-            v-if="!nav.module"
-            v-for="s in m.children"
-            :key="s.url"
-            :href="s.url"
+            v-for="a in nav.section.children"
+            :key="a.url"
+            :href="a.url"
+            :class="{ active: a == nav.article }"
           >
-            <span class="block ml-4">
-              {{ s.text }}
+            <span class="block ml-8">
+              {{ a.text }}
             </span>
           </a>
+          <div class="ml-8">
+            <slot />
+          </div>
         </template>
       </template>
-      <a v-if="nav.module" class="mt-8" :href="nav.home.url">
-        {{ nav.home.text }}
-      </a>
+
+      <div class="mb-8" />
+    </template>
+
+    <template v-for="m in nav.home.children" :key="m.url">
+      <template v-if="m != nav.module">
+        <a :href="m.url">
+          {{ m.text }}
+        </a>
+        <a
+          v-if="!nav.module"
+          v-for="s in m.children"
+          :key="s.url"
+          :href="s.url"
+        >
+          <span class="block ml-4">
+            {{ s.text }}
+          </span>
+        </a>
+      </template>
     </template>
   </nav>
 </template>
