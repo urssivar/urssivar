79f15e3a8458aacd3531218cf20c519ff97ec21c
Mæhæmad <alkaitagi@outlook.com>
2026-06-19 19:31:15 +0000

    Add `Noto Sans Arabic` font fallback

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -30,7 +30,9 @@ export default defineConfig({
     ["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" }],
     ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" }],
     ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" }],
-    ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+Math&display=swap" }]
+    ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@100..900&display=swap" }],
+    ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+Math&display=swap" }],
+
   ],
 
   transformPageData,
